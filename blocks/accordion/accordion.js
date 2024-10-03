a535dfb55c5d43a1327373dcce39c0ff54cb3375
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-10-03 10:18:07 +0900

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,6 +1,10 @@
 export default function decorate(block) {
-    const accordionDiv = block.querySelector(':scope > div > div');
+    const accordionDiv = block.querySelector(':scope > div');
     const inputElm = document.createElement('input');
     inputElm.classList.add('toggle');
+    inputElm.setAttribute('type','checkbox');
+    const id = crypto.randomUUID();
+    inputElm.setAttribute('id',id);
+
     accordionDiv.appendChild( inputElm );
 }
