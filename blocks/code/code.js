b78d006022bbaa206d4f16676f794ba1fffecb1e
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2025-04-18 06:28:49 +0900

    Update code.js

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
     // decorate the code as a code element
     const codeDivs = block.querySelectorAll(':scope > div > div');
+    console.log(codeDivs);
     codeDivs.forEach(codeDiv, index => {
       const code = document.createElement('code');
       code.innerHTML = `${codeDiv.innerHTML}`;
