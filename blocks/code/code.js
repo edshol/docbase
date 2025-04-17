f06c990386f618704e1ba7ec591eeaa611853fee
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2025-04-18 06:34:29 +0900

    Update code.js

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -2,11 +2,11 @@ export default function decorate(block) {
     // decorate the code as a code element
     const codeDivs = block.querySelectorAll(':scope > div > div');
     console.log(codeDivs);
-    codeDivs.forEach(codeDiv, index => {
-      console.log("code "+index);
-      console.log(codeDiv.innerText);
+    codeDivs.forEach( (codeDiv, index) => {
       const code = document.createElement('code');
       code.innerHTML = `${codeDiv.innerHTML}`;
       codeDiv.parentElement.replaceWith(code);
+      console.log("code "+index);
+      console
     });
 }
